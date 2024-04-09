1498916ebf1da74d35ae482c9d8a7ed4062db9eb
Ritwik Goel <ritwikgoel@yahoo.com>
2024-04-09 01:23:22 -0400

    New Channel for sending data

--- app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
@@ -2,67 +2,32 @@ package com.example.app
 
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.PrintWriter
 
 class MainActivity : FlutterActivity() {
-
-    private val FLUTTER_CHANNEL = "tee_channel"
-    private val TEE_CHANNEL = "com.example.app.tee"
+    private val CHANNEL = "tee_channel"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Set up method channel to communicate with Flutter
-        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, FLUTTER_CHANNEL)
+        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL)
             .setMethodCallHandler { call, result ->
-                when (call.method) {
-                    "processImageData" -> {
-                        val imageData = call.argument<ByteArray>("data")
-                        if (imageData != null) {
-                            // Handle image data here
-                            println("Workig")
-                            sendDataToTEE(imageData)
-                            result.success(null) // Indicate success back to Flutter
-                        } else {
-                            result.error("INVALID_DATA", "Image data is null", null)
-                        }
+                if (call.method == "processImageData") {
+                    val imageData = call.argument<ByteArray>("data")
+                    if (imageData != null) {
+                        // Handle image data here
+                        // Send it to the TEE or perform any other processing
+                        println("Success!!!!!!!!!!!!!!!!!!!!!")
+                        println("Image Data: ${imageData.contentToString()}") 
+                        
+                        result.success(null) // Indicate success back to Flutter
+                    } else {
+                        println("NO Success!!!!!!!!!!!!!!!!!!!!!")
+
+                        result.error("INVALID_DATA", "Image data is null", null)
                     }
-                    else -> result.notImplemented()
+                } else {
+                    result.notImplemented()
                 }
             }
-    }
-
-    private fun sendDataToTEE(data: ByteArray) {
-        try {
-            val pfd = openParcelFileDescriptor(TEE_CHANNEL, null)
-            val fis = FileInputStream(pfd.fileDescriptor)
-            val fos = FileOutputStream(pfd.fileDescriptor)
-            val writer = PrintWriter(fos)
-
-            // Send data to TEE
-            sendData(writer, data)
-
-            // Close streams
-            writer.close()
-            fis.close()
-            fos.close()
-            pfd.close()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun sendData(writer: PrintWriter, data: ByteArray) {
-        writer.println("sendData") // Method name defined in the TEE interface
-        writer.println(data.size) // Send size of data
-        writer.flush()
-
-        // Write data to the stream
-        writer.write(data.decodeToString())
-        writer.flush()
     }
 }
