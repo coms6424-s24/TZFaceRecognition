1352aaf6ea1b95d6c966c31ff9192ac06e221fc3
Ritwik Goel <ritwikgoel@yahoo.com>
2024-04-24 14:15:11 -0400

    Feat: FAce Recog using opencv in c

---
+++ SecureZone/face_recognition/build/CMakeFiles/face_recognition.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/face_recognition.dir/src/main.c.o"
+  "CMakeFiles/face_recognition.dir/src/main.c.o.d"
+  "face_recognition"
+  "face_recognition.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/face_recognition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SecureZone/face_recognition/src/main.c
@@ -0,0 +1,7 @@
+#include <opencv2/opencv.hpp>
+#include<stdio.h>
+
+int main() {
+    // Your face recognition algorithm code using OpenCV
+    return 0;
+}
